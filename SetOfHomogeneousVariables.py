37113c70e3ca6cb4c9fb8d7d6c811d6f6c99f365
LHC scrubbing <lhcscrub@lxplus085.cern.ch>
2016-12-02 11:26:48 +0100

    Nicer transformation to np arrays and fixed self.aligned

--- SetOfHomogeneousVariables.py
+++ SetOfHomogeneousVariables.py
@@ -7,21 +7,30 @@ class SetOfHomogeneousNumericVariables:
         self.variable_list = variable_list
 
         if type(timber_variables) is str:
-            dict_timber = tm.parse_timber_file(timber_variables, verbose=True)
+            dict_timber = tm.parse_timber_file(timber_variables, verbose=False)
         elif hasattr(timber_variables, '__getitem__'):
             dict_timber = timber_variables
 
         self.timber_variables = {}
         for var_name in self.variable_list:
              self.timber_variables[var_name] = dict_timber[var_name]
-             self.timber_variables[var_name].t_stamps = np.float_(np.array(self.timber_variables[var_name].t_stamps))
-             self.timber_variables[var_name].values = np.float_(np.squeeze(np.array(self.timber_variables[var_name].values)))
+             self.timber_variables[var_name].t_stamps = np.array(self.timber_variables[var_name].t_stamps,dtype=float)
+
+             self.timber_variables[var_name].values = np.array(self.timber_variables[var_name].values, dtype=float)
+             shape = self.timber_variables[var_name].values.shape
+             if len(shape) > 1 and shape[1] == 1:
+                 self.timber_variables[var_name].values = np.squeeze(self.timber_variables[var_name].values, axis=1)
 
     def aligned(self):
         aligned_list = []
         t_first = self.timber_variables[self.variable_list[0]].t_stamps
         for ii,kk in enumerate(self.variable_list):
-            aligned_list.append(np.interp(t_first, self.timber_variables[kk].t_stamps, self.timber_variables[kk].values))
+            values = self.timber_variables[kk].values
+            t_stamps = self.timber_variables[kk].t_stamps
+            if len(values) == 0:
+                aligned_list.append(np.zeros_like(t_first))
+            else:
+                aligned_list.append(np.interp(t_first, t_stamps, values))
         return t_first, np.array(aligned_list)
 
     def aligned_object(self):
