ae806f11c38d44267a36378db418f4839bd2fd5f
LHC scrubbing <lhcscrub@lxplus048.cern.ch>
2017-01-15 20:20:16 +0100

    nicer interface to plt.figure, combined legends and better colorprog

--- mystyle.py
+++ mystyle.py
@@ -2,6 +2,7 @@ from matplotlib import rc, rcdefaults
 import pylab as pl
 from colorsys import hsv_to_rgb
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def mystyle(fontsz=16):
@@ -29,5 +30,25 @@ def scix():
 
 
 def colorprog(i_prog, Nplots, v1 = .9, v2 = 1.):
-	#return [pl.cm.rainbow(k) for k in np.linspace(0, 1, Nplots)][i_prog]
-	return hsv_to_rgb(float(i_prog)/float(Nplots), v1, v2)
+    if hasattr(Nplots, '__len__'):
+	    Nplots = len(Nplots)
+    return hsv_to_rgb(float(i_prog)/float(Nplots), v1, v2)
+    #return [pl.cm.rainbow(k) for k in np.linspace(0, 1, Nplots)][i_prog]
+
+def comb_legend(sp1, sp2, *args, **kwargs):
+    """
+    Combine legends for twinx()ed subplots
+    """
+    lines, labels = sp1.get_legend_handles_labels()
+    lines2, labels2 = sp2.get_legend_handles_labels()
+    sp2.legend(lines + lines2, labels + labels2, *args, **kwargs)
+
+def figure(title, figs=None):
+    fig = plt.figure()
+    fig.canvas.set_window_title(title)
+    if figs != None:
+        figs.append(fig)
+    fig.patch.set_facecolor('w')
+    plt.suptitle(title, fontsize=20)
+    return fig
+
